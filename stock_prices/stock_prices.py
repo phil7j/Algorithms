501c3fc0a3eaa62535161bcd9fb2f1dfaa343da5
Philip Johnson <phil7j@gmail.com>
2019-11-06 19:01:36 -0800

    finished stock_prices algo

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -4,18 +4,30 @@ import argparse
 
 
 def find_max_profit(prices):
-  # Find biggest difference of prices Buy comes before Sell
-  # Idea 1:
-  # Iterate with 2 points over prices array:
-  # First pointer selects the first number
-  # Second pointer iterates over the rest of the prices and pushes the differences to a new array
-  # After going through, and recording the data, First pointer goes to the next value and process starts all over
-  # All differences in the array get checked to see which one is largest
-  # Return number
+    # Find biggest difference of prices Buy comes before Sell
+    # Idea 1:
+    # Iterate with 2 points over prices array:
+    # First pointer selects the first number and moves right after every completed for loop
+    # Second pointer iterates over the prices that are to the right of it in the array and puts the highest difference in a variable
+    # After going through, and recording the data, First pointer goes to the next value and process starts all over
+    # All differences in the array get checked to see which one is largest
+    # Return number
+    profit = -99999
+
+    for i in range(0, len(prices) - 2):
+        for j in range(i+1, len(prices)-1):
+            diff = prices[j] - prices[i]
+            if diff > profit:
+                profit = diff
+    return profit
+
+
+print(find_max_profit([100, 55, 4, 98, 10, 18,
+                       90, 95, 43, 11, 47, 67, 89, 42, 49, 79]))
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
+    # This is just some code to accept inputs from the command line
     parser = argparse.ArgumentParser(
         description='Find max profit from prices.')
     parser.add_argument('integers', metavar='N', type=int,
